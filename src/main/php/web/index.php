cda61ecd9d57b35a09a5f197eebed2ce47ac1c2a
Wellington Lucas <wellington.tylon@hotmail.com>
2015-03-10 00:01:18 -0300

    Adicionados arquivos de estilo e estruturação do projeto

---
+++ src/main/php/web/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="icon" href="../../resources/imagens/logo_ufc.png" type="image/png" sizes="16x16">
+		<link rel="stylesheet" href="../../resources/css/bootstrap-3.3.2-dist/css/bootstrap.css">
+		<title>To_do_list</title>
+	</head>
+	<body>	
+			
+		<div class="container" data-example-id="contextual-table">
+			<table class="table table-bordered">
+		      <thead>
+		        <tr>
+		          <th>#</th>
+		          <th>Column heading</th>		          
+		        </tr>
+		      </thead>
+		      <tbody>
+		        <tr class="active">
+		          <th scope="row">1</th>
+		          <td>Column content</td>		       
+		        </tr>
+		        <tr>
+		          <th scope="row">2</th>
+		          <td>Column content</td>		          
+		        </tr>
+		        <tr class="success">
+		          <th scope="row">3</th>
+		          <td>Column content</td>		          
+		        </tr>
+		        <tr>
+		          <th scope="row">4</th>
+		          <td>Column content</td>		          
+		        </tr>
+		        <tr class="info">
+		          <th scope="row">5</th>
+		          <td>Column content</td>		          
+		        </tr>
+		        <tr>
+		          <th scope="row">6</th>
+		          <td>Column content</td>		          
+		        </tr>
+		        <tr class="warning">
+		          <th scope="row">7</th>
+		          <td>Column content</td>		          
+		        </tr>
+		        <tr>
+		          <th scope="row">8</th>
+		          <td>Column content</td>		          
+		        </tr>
+		        <tr class="danger">
+		          <th scope="row">9</th>
+		          <td>Column content</td>		          
+		        </tr>
+		      </tbody>
+		    </table>				
+			
+			
+		</div>
+		
+	</body>
+</html>
+	
